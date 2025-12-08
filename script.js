fe8797a851325130ac6c1c7ec173ec9758ca2dc7
AngelCobo18 <angelbitrian0@gmail.com>
2025-12-08 15:50:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,19 @@
 // ===========================
 const citas = [
     // --------------------- DEFREDS ---------------------
-    {texto: "Por el día sobrevivía a base de amigos y poesía. Pero cada noche era una agonía...", autor: "Defreds", obra: "Casi sin querer"},
+    {texto: "Por el día sobrevivía a base de amigos y poesía. Pero cada noche era una agonía. Deseaba que no llegara nunca, para no tener que tumbarme y pensar en su melena.", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Como llorar sin ganas. Como follar sin ansias. Como la vida sin ti.", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Te odio, te quiero.", autor: "Defreds", obra: "Casi sin querer"},
-    {texto: "Esa persona que da igual el tiempo que pase, da igual dónde vivas y con quién...", autor: "Defreds", obra: "Casi sin querer"},
+    {texto: "Esa persona que da igual el tiempo que pase, da igual dónde vivas y con quién. Esa que recuerdas en cualquier lugar inesperado. Y que solo con recordarla eres capaz de temblar.", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Habla mucho. Justo lo que calla es lo que no se puede saber.", autor: "Defreds", obra: "Casi sin querer"},
-    {texto: "Si te mirara a los ojos una vez más, no me atrevería a decir nada...", autor: "Defreds", obra: "Casi sin querer"},
+    {texto: "Si te mirara a los ojos una vez más, no me atrevería a decir nada, quizá te abrazaría. O lloraría, no lo sé.", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Que, aunque tú no lo sabías, yo en el fondo te esperaba.", autor: "Defreds", obra: "Casi sin querer"},
-    {texto: "Pero he de confesarte que nunca he disfrutado tanto como entre tus brazos...", autor: "Defreds", obra: "Casi sin querer"},
-    {texto: "Ver que ese desconocido, en algún momento, fue tu mayor conocido...", autor: "Defreds", obra: "Casi sin querer"},
+    {texto: "Pero he de confesarte que nunca he disfrutado tanto como entre tus brazos: ese fue mi viaje preferido.", autor: "Defreds", obra: "Casi sin querer"},
+    {texto: "Ver que ese desconocido, en algún momento, fue tu mayor conocido. Y sobretodo, saber que dos personas que se recuerdan cuando ni hablan son capaces de todo.", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Y aunque no lo creas, sus silencios dicen más que el resto del mundo a gritos.", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Respiro. Pero ya no vivo...", autor: "Defreds", obra: "Casi sin querer"},
     {texto: "Algunas veces recuerda aquello que pudo ser y dejó pasar por miedo.", autor: "Defreds", obra: "Casi sin querer"},
-    {texto: "Y ahora quiero un billete para huir de recuerdos...", autor: "Defreds", obra: "Casi sin querer"},
+    {texto: "Y ahora quiero un billete para huir de recuerdos. Y sale demasiado caro. Dicen que está agotado.", autor: "Defreds", obra: "Casi sin querer"},
 
     // --------------------- GABRIEL GARCÍA MÁRQUEZ ---------------------
     {texto: "No hay un anciano que olvide dónde escondió su tesoro.", autor: "Gabriel García Márquez", obra: "Memoria de mis putas tristes"},
@@ -25,15 +25,15 @@ const citas = [
     {texto: "Es imposible no terminar siendo como los otros creen que uno es.", autor: "Gabriel García Márquez", obra: "Memoria de mis putas tristes"},
     {texto: "No hay peor desgracia que morir solo.", autor: "Gabriel García Márquez", obra: "Memoria de mis putas tristes"},
 
-    // Duplicadas para Cicerón y Julio César
+    // Citas duplicadas para Cicerón y Julio César sin obra
     {texto: "No hay un anciano que olvide dónde escondió su tesoro.", autor: "Cicerón", obra: ""},
     {texto: "Es imposible no terminar siendo como los otros creen que uno es.", autor: "Julio César", obra: ""},
 
     // --------------------- HEMINGWAY ---------------------
-    {texto: "Pensó que aburrimiento era una palabra que ningún campesino del mundo usaría...", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"},
-    {texto: "Quiero lo que tú quieras. Quiero hacerlo todo...", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"},
-    {texto: "La llegada de la noche le hacía sentirse siempre más solo...", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"},
-    {texto: "Pero un hombre inteligente se ve obligado a emborracharse algunas veces...", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"}
+    {texto: "Pensó que aburrimiento era una palabra que ningún campesino del mundo usaría en ninguna otra lengua. Y sin embargo, es la palabra más corriente en boca de un español de cualquier clase.", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"},
+    {texto: "Quiero lo que tú quieras. Quiero hacerlo todo, y si lo hacemos todo, quizá sea como si lo otro no hubiese ocurrido.", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"},
+    {texto: "La llegada de la noche le hacía sentirse siempre más solo, y aquella noche se sentía tan solo, que se había hecho dentro de él un vacío como si fuera de hambre.", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"},
+    {texto: "Pero un hombre inteligente se ve obligado a emborracharse algunas veces para poder pasar el tiempo con los imbéciles.", autor: "Ernest Hemingway", obra: "Por quién doblan las campanas"}
 ];
 
 // ===========================
@@ -254,5 +254,6 @@ function mostrarInicioDestacados() {
 
 // ================= INICIO AUTOMÁTICO =================
 citaDelDia();
+
 
 
